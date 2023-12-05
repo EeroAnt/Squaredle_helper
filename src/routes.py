b0936cad1c17c93aa8709052a2fb830afc14ebfc
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-12-05 20:34:00 +0200

    init wordservice

--- src/routes.py
+++ src/routes.py
@@ -1,12 +1,18 @@
 from app import app
 from flask import render_template
-from src.services.create_grid import _get_grid_size
+from src.services.gridservice import _get_grid_size
+from src.services.wordservice import _get_words
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/create_grid", methods=["POST"])
+@app.route("/submit_grid", methods=["POST"])
 def create_grid():
     size = _get_grid_size()
     return render_template("submit_grid.html", size=size)
+
+@app.route("/words", methods=["POST"])
+def submit_grid():
+    words = _get_words()
+    return render_template("words.html", words=words)

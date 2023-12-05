0b406b0908edd0ebab043c86047ae33fd90ca094
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-12-05 11:19:30 +0200

    init create_grid-function

--- src/routes.py
+++ src/routes.py
@@ -1,7 +1,11 @@
 from app import app
 from flask import render_template
+from src.services.create_grid import create_grid
 
 @app.route("/")
 def index():
     return render_template("index.html")
-    
+
+@app.route("/create_grid")
+def create_grid():
+    create_grid()
